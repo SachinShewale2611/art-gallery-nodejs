cc0ef939296a80acb76d87a3bcb06887ead495cc
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-21 12:55:36 +0000

    Add catch async.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,6 @@ const userController = require("./../controllers/userController");
 const router = express.Router();
 
 router.route("/").get(userController.getAllUsers);
+router.route("/").post(userController.createUser);
 
 module.exports = router;
