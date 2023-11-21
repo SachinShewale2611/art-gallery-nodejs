d1d66e989dc1f228a02dd4d6f96a71174a1c168c
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-21 12:22:42 +0000

    Database connect.

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
-
 // Import the required modules
-const express = require('express');
+const express = require("express");
 
 // Import the mongoose module
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 //Import the dotenv module
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-dotenv.config({ path: './config.env' });
+dotenv.config({ path: "./.env" });
 
 //import app.js
-const app = require('./app');
+const app = require("./app");
 
-
+// Connect to the database
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 // Start the server
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION! 💥 Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
