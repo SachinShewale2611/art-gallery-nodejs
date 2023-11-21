3ee6dda562a3ee7b27dfd363606534d6604c08f7
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-21 12:23:06 +0000

    Format code.

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const path = require('path');
-const express = require('express');
-const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const hpp = require('hpp');
-const { xss } = require('express-xss-sanitizer');
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+const { xss } = require("express-xss-sanitizer");
 
-const userRouter = require('./routes/userRoutes');
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
 // 1) GLOBAL MIDDLEWARES
 // Serving static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Set security HTTP headers
 app.use(helmet());
 
 // Development logging
-if (process.env.NODE_ENV === 'development') { 
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // Limit requests from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: "Too many requests from this IP, please try again in an hour!",
 });
-app.use('/api', limiter);
+app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
-app.use(express.json({ limit: '10kb' }));
-app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
+app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
@@ -42,19 +42,19 @@ app.use(mongoSanitize());
 app.use(xss());
 
 // Prevent parameter pollution
-app.use(hpp()); 
+app.use(hpp());
 
 // Define a route
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/users", userRouter);
 
 // Handle unhandled routes
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server!`
+    status: "fail",
+    message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
 
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,8 @@
-const express = require('express');
-const userController = require('./../controllers/userController');
-
+const express = require("express");
+const userController = require("./../controllers/userController");
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(userController.getAllUsers);
+router.route("/").get(userController.getAllUsers);
 
-
-  module.exports = router;
+module.exports = router;
