97aa58f4d34349c01e3fc94032df767d4d20261c
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-22 06:30:46 +0000

    Add signup & login methods.

---
+++ controllers/authController.js
@@ -0,0 +1,58 @@
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/appError");
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  // Remove password from output
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: "success",
+    token,
+    data: {
+      user,
+    },
+  });
+};
+const signToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+};
+
+exports.signup = catchAsync(async (req, res, next) => {
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  //   const url = `${req.protocol}://${req.get("host")}/me`;
+  //   console.log(url);
+  //   await new Email(newUser, url).sendWelcome();
+
+  createSendToken(newUser, 201, res);
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user exists && password is correct
+  const user = await User.findOne({ email }).select('+password');
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  // 3) If everything ok, send token to client
+  createSendToken(user, 200, res);
+});
