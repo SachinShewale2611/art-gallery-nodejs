3d5670c838902b012683e2e96117adad2c0bceb4
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-22 06:30:10 +0000

    Add globle error handler.

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const hpp = require("hpp");
 const { xss } = require("express-xss-sanitizer");
 
 const userRouter = require("./routes/userRoutes");
+const globalErrorHandler = require("./controllers/errorController");
 
 const app = express();
 
@@ -24,12 +25,12 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Limit requests from same API
-const limiter = rateLimit({
-  max: 100,
-  windowMs: 60 * 60 * 1000,
-  message: "Too many requests from this IP, please try again in an hour!",
-});
-app.use("/api", limiter);
+// const limiter = rateLimit({
+//   max: 100,
+//   windowMs: 60 * 60 * 1000,
+//   message: "Too many requests from this IP, please try again in an hour!",
+// });
+// app.use("/api", limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
@@ -57,5 +58,7 @@ app.all("*", (req, res, next) => {
     message: `Can't find ${req.originalUrl} on this server!`,
   });
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
