8f0c0e3aa451b0d8079050a1d39d8d9ecb580ca6
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-22 12:50:50 +0000

    Add forgot password and password reset methods.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,10 +5,15 @@ const authController = require("./../controllers/authController");
 const router = express.Router();
 
 
+
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
+// router.get('/logout', authController.logout);
 
-router.route("/").get(userController.getAllUsers);
+router.post('/forgotPassword', authController.forgotPassword);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+router.route("/").get(authController.protect, userController.getAllUsers);
 // router.route("/").post(userController.createUser);
 
 module.exports = router;
