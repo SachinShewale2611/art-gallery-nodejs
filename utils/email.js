5d77dfdf47e2b1b7850ecdf8650ec9756f3ecc1c
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-22 12:50:42 +0000

    Add email class.

---
+++ utils/email.js
@@ -0,0 +1,59 @@
+const nodemailer = require("nodemailer");
+const htmlToText = require("html-to-text");
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(" ")[0];
+    this.url = url;
+    this.from = `Jonas Schmedtmann <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === "production") {
+      // Sendgrid
+      return nodemailer.createTransport({
+        service: "SendGrid",
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD,
+        },
+      });
+    }
+
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(subject) {
+    // 1) Create HTML with only the URL
+    const html = `<p>${this.url}</p>`;
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.htmlToText(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send("Welcome!");
+  }
+
+  async sendPasswordReset() {
+    await this.send("Your password reset token (valid for only 10 minutes)");
+  }
+};
