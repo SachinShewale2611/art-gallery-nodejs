7c999ccbc995d9cd00a7b53f6345f59d69b18421
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-28 08:51:50 +0000

    Format code.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,12 @@ const authController = require("./../controllers/authController");
 
 const router = express.Router();
 
-
-
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
 // router.get('/logout', authController.logout);
 
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 router.route("/").get(authController.protect, userController.getAllUsers);
 // router.route("/").post(userController.createUser);
