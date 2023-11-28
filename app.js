944ba2c73e574b1ae49accf259aa33c056d91845
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-28 06:11:27 +0000

    Add rate limiter.

--- app.js
+++ app.js
@@ -24,13 +24,16 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.set('trust proxy', 1); // Trust the first hop of the proxy
+
 // Limit requests from same API
-// const limiter = rateLimit({
-//   max: 100,
-//   windowMs: 60 * 60 * 1000,
-//   message: "Too many requests from this IP, please try again in an hour!",
-// });
-// app.use("/api", limiter);
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	headers: true, // Send custom rate limit header with limit and remaining
+})
+app.use(limiter);
+
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
