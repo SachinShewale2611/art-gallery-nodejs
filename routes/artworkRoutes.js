089d8ba3b00415cd2cfd9daa67fc33731d231432
Sachin Shewale <110060861+SachinShewale2611@users.noreply.github.com>
2023-11-29 09:22:31 +0000

    Add nested route for artwork.

--- routes/artworkRoutes.js
+++ routes/artworkRoutes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const router = express.Router();
 const artworkController = require("../controllers/artworkController");
 const authController = require("../controllers/authController");
 
